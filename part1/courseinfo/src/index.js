82b9b6f8923b2d1adf64e68d0a86102fbe37a0a4
Gee-Bee <greg.bunia@gmail.com>
2019-08-27 17:03:26 +0200

    1.1: course information, step1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,19 +12,31 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header title={course} />
+      <Content part1={part1} excercises1={exercises1}
+               part2={part2} excercises2={exercises2}
+               part3={part3} excercises3={exercises3} />
+      <Total text="Number of excercises" value={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
+
+const Header = (props) => (
+  <h1>{props.title}</h1>
+)
+
+const Content = (props) => (
+  <>
+    <p>{props.part1} {props.excercises1}</p>
+    <p>{props.part2} {props.excercises2}</p>
+    <p>{props.part3} {props.excercises3}</p>
+  </>
+)
+
+const Total = (props) => (
+  <p>
+    {props.text} {props.value}
+  </p>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
