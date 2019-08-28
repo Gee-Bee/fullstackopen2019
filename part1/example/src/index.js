39898662cd747199e435686c03d78f00ec2ba88d
Gee-Bee <greg.bunia@gmail.com>
2019-08-28 13:33:16 +0200

    part1 example: page re-rendering

--- part1/example/src/index.js
+++ part1/example/src/index.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+const App = ({ counter }) => {
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      {counter}
     </div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
+let counter = 0;
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
+const refresh = () => {
+  ReactDOM.render(
+    <App counter={counter} />,
+    document.getElementById('root')
+  );
+
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+setInterval(() => {
+  refresh();
+  counter += 1;
+}, 1000)
