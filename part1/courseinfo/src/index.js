ebd1edecfc20b2e9b53a61358d72ff004ff3e5a5
Gee-Bee <greg.bunia@gmail.com>
2019-08-28 11:13:40 +0200

    1.5: course information step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,30 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
-  const totalValue = parts
-    .map(a => a.exercises)
-    .reduce((a, b) => (a + b), 0);
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <>
-      <Header text={course} />
-      <Content parts={parts} />
-      <Total text="Number of excercises" value={totalValue} />
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total text="Number of excercises" parts={course.parts} />
     </>
   )
 }
@@ -34,9 +33,8 @@ const Header = ({ text }) => (
   <h1>{text}</h1>
 )
 
-const Content = ({ parts: [ part1, part2, part3 ] }) => {
-  // const [ part1, part2, part3 ] = props.parts;
-  return [ part1, part2, part3].map(part => (
+const Content = ({ parts }) => {
+  return parts.map(part => (
     <Part key={part.name}
           name={part.name}
           exercises={part.exercises} />
@@ -47,10 +45,16 @@ const Part = ({ name, exercises }) => (
   <p>{name} {exercises}</p>
 )
 
-const Total = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
-)
+const Total = ({ text, parts }) => {
+  const totalExercises = parts
+    .map(a => a.exercises)
+    .reduce((a,b) => a + b, 0);
+
+  return (
+    <p>
+      {text} {totalExercises}
+    </p>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
