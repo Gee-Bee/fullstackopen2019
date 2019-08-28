6fed8b5f391bcbc7a14149522c13abf256ce6b71
Gee-Bee <greg.bunia@gmail.com>
2019-08-28 11:01:46 +0200

    1.4: course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,28 +3,30 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
-  const totalValue = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+  const totalValue = parts
+    .map(a => a.exercises)
+    .reduce((a, b) => (a + b), 0);
 
   return (
-    <div>
+    <>
       <Header text={course} />
-      <Content part1={part1}
-               part2={part2}
-               part3={part3} />
+      <Content parts={parts} />
       <Total text="Number of excercises" value={totalValue} />
-    </div>
+    </>
   )
 }
 
@@ -32,15 +34,13 @@ const Header = ({ text }) => (
   <h1>{text}</h1>
 )
 
-const Content = (props) => {
-  const { part1, part2, part3 } = props;
-  return (
-    <>
-      <Part name={part1.name} exercises={part1.exercises} />
-      <Part name={part2.name} exercises={part2.exercises} />
-      <Part name={part3.name} exercises={part3.exercises} />
-    </>
-  );
+const Content = ({ parts: [ part1, part2, part3 ] }) => {
+  // const [ part1, part2, part3 ] = props.parts;
+  return [ part1, part2, part3].map(part => (
+    <Part key={part.name}
+          name={part.name}
+          exercises={part.exercises} />
+  ));
 }
 
 const Part = ({ name, exercises }) => (
