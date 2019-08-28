38195c61fb94cc0ba6df744e5773fa71547fe134
Gee-Bee <greg.bunia@gmail.com>
2019-08-28 10:26:43 +0200

    1.3: course information step3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,43 +3,53 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  }
+  const totalValue = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
-      <Header title={course} />
-      <Content part1={part1} exercises1={exercises1}
-               part2={part2} exercises2={exercises2}
-               part3={part3} exercises3={exercises3} />
-      <Total text="Number of excercises" value={exercises1 + exercises2 + exercises3} />
+      <Header text={course} />
+      <Content part1={part1}
+               part2={part2}
+               part3={part3} />
+      <Total text="Number of excercises" value={totalValue} />
     </div>
   )
 }
 
-const Header = (props) => (
-  <h1>{props.title}</h1>
+const Header = ({ text }) => (
+  <h1>{text}</h1>
 )
 
-const Content = (props) => (
-  <>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
-  </>
+const Content = (props) => {
+  const { part1, part2, part3 } = props;
+  return (
+    <>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
+    </>
+  );
+}
+
+const Part = ({ name, exercises }) => (
+  <p>{name} {exercises}</p>
 )
 
-const Part = (props) => (
-  <p>{props.part} {props.exercises}</p>
-)
-
-const Total = (props) => (
+const Total = ({ text, value }) => (
   <p>
-    {props.text} {props.value}
+    {text} {value}
   </p>
 )
 
