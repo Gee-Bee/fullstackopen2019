33b7ec426c8ad1a98fd49273a0c2304951a3ac5d
Gee-Bee <greg.bunia@gmail.com>
2019-08-29 10:22:38 +0200

    1.10: unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,9 +1,28 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({ good, neutral, bad }) => {
   const header = <h1>statistics</h1>;
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+  if (good === 0 && neutral === 0 && bad === 0) {
     return (
       <>
         {header}
@@ -11,40 +30,32 @@ const Statistics = (props) => {
       </>
     )
   }
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positive = good / all * 100;
   return (
     <>
       {header}
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
-      <div>all {props.all}</div>
-      <div>average {props.average}</div>
-      <div>positive {props.positive} %</div>
+      <table>
+        <tbody>
+          <Statistic name="good" value={good} />
+          <Statistic name="neutral" value={neutral} />
+          <Statistic name="bad" value={bad} />
+          <Statistic name="all" value={all} />
+          <Statistic name="average" value={average} />
+          <Statistic name="positive" value={`${positive} %`} />
+        </tbody>
+      </table>
     </>
   )
 };
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const all = good + neutral + bad;
-  const average = (good * 1 + bad * -1) / all;
-  const positive = good / all * 100;
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad}
-        all={all} average={average} positive={positive} />
-    </div>
-  )
-};
+const Statistic = ({ name, value}) => (
+  <tr>
+    <td>{name}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 ReactDOM.render(<App />,
   document.getElementById('root')
