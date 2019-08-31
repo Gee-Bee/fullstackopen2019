152a57ffcbadbf87ac3a2b430ef233c6cbe37162
Gee-Bee <greg.bunia@gmail.com>
2019-08-31 22:13:36 +0200

    part2 example: npm dependencies - json-server, axios

--- part2/example/src/index.js
+++ part2/example/src/index.js
@@ -24,6 +24,15 @@ const notes = [
   }
 ]
 
+setTimeout(() => {
+  console.log('loop..')
+  let i = 0
+  while (i < 50000000000) {
+    i++
+  }
+  console.log('end')
+}, 5000)
+
 
 ReactDOM.render(
   <App notes={notes} />,
