429262498670cdeb510c8e132ef8a9a2e972aed9
Gee-Bee <greg.bunia@gmail.com>
2019-09-10 19:04:13 +0200

    part3 example: get, delete note

--- part3/example-backend/index.js
+++ part3/example-backend/index.js
@@ -30,6 +30,22 @@ app.get('/notes', (req, res) => {
   res.json(notes);
 });
 
+app.get('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter(note => note.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
